3344ff226aac298ce2ea627291cf67122bf5ad93
Rob Labs <roblabs@users.noreply.github.com>
2022-04-11 16:54:23 -0700

    Allow API customers to set the UnitTemperature to get either US or SI (metric) results from the API (#9)

--- Tests/NationalWeatherServiceTests/integration/GetForecastIntegrationTests.swift
+++ Tests/NationalWeatherServiceTests/integration/GetForecastIntegrationTests.swift
@@ -2,6 +2,7 @@ import XCTest
 @testable import NationalWeatherService
 
 final class GetForecastIntegrationTests: XCTestCase {
+    /// Get forecast without setting `NationalWeatherService.units`
     func testGetForecastForLocation() throws {
         let forecastExpectation = self.expectation(description: "get forecast expectation")
         nws.forecast(latitude: 47.6174, longitude: -122.2017) { result in
@@ -15,7 +16,44 @@ final class GetForecastIntegrationTests: XCTestCase {
 
         wait(for: [forecastExpectation], timeout: 5)
     }
+    
+    /// Get forecast by setting `NationalWeatherService.units` to `.fahrenheit`
+    func testGetForecastFahrenheit() throws {
+        let forecastExpectation = self.expectation(description: "get forecast expectation")
+        
+        NationalWeatherService.units = .fahrenheit
+        nws.forecast(latitude: 47.6174, longitude: -122.2017) { result in
+            XCTAssertSuccess(result)
 
+            let forecast = try! result.get()
+            print(forecast)
+            XCTAssertFalse(forecast.periods.isEmpty)
+
+            forecastExpectation.fulfill()
+        }
+
+        wait(for: [forecastExpectation], timeout: 5)
+    }
+    
+    /// Get forecast by setting `NationalWeatherService.units` to `.celsius`
+    func testGetForecastCelsius() throws {
+        let forecastExpectation = self.expectation(description: "get forecast expectation")
+        
+        NationalWeatherService.units = .celsius
+        nws.forecast(latitude: 47.6174, longitude: -122.2017) { result in
+            XCTAssertSuccess(result)
+
+            let forecast = try! result.get()
+            
+            print(forecast)
+            XCTAssertFalse(forecast.periods.isEmpty)
+
+            forecastExpectation.fulfill()
+        }
+
+        wait(for: [forecastExpectation], timeout: 5)
+    }
+    
     func testGetHourlyForecast() throws {
         let hourlyForecastExpectation = self.expectation(description: "get hourly forecast expectation")
         nws.hourlyForecast(latitude: 47.6174, longitude: -122.2017) { result in
